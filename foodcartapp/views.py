100e5508aa5795d752428f612c3e9c3656e3de48
Denischerov2312 <107198033+Denischerov2312@users.noreply.github.com>
2026-05-26 21:36:13 +0300

    Добавил валидацию ошибок

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,6 +1,6 @@
-import json
 from django.http import JsonResponse
 from django.templatetags.static import static
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -57,24 +57,51 @@ def product_list_api(request):
     return Response(dumped_products)
 
 
+@api_view(['POST'])
 def register_order(request):
+    data = request.data
+
+    if 'products' not in data:
+        return Response({'products': 'Обязательное поле'}, status=status.HTTP_400_BAD_REQUEST)
+    products = data['products']
+    if not isinstance(products, list):
+        return Response(
+            {'products': f'Должен быть type:list, но получено {type(products).__name__}'},
+            status=status.HTTP_400_BAD_REQUEST)
+    if not products:
+        return Response({'products': 'Не пустое поле'}, status=status.HTTP_400_BAD_REQUEST)
+
+    for item in products:
+        quantity = item['quantity']
+        if type(quantity) is not int:
+            return Response(
+                {'products': f'Количество должно быть числом(int)'},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        if quantity < 1:
+            return Response(
+                {'products': 'Количетсво товаров должно быть больше 0'},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
     try:
-        data = json.loads(request.body.decode())
         order = Order.objects.create(
             firstname=data['firstname'],
             lastname=data['lastname'],
             phonenumber=data['phonenumber'],
             adress=data['address'],
-                             )
-        for item in data.get('products', []):
+                                )
+        for item in products:
             OrderItem.objects.create(
                 order=order,
                 product_id=item['product'],
                 quantity=item['quantity'],
             )
 
-        return JsonResponse({'status': 'success'}, status=200)
-    except ValueError:
-        return JsonResponse({
-            'error': 'bla bla bla',
-        })
+        return JsonResponse({'status': 'success'}, status=status.HTTP_200_OK)
+    except KeyError as error:
+        return Response({
+            'error': f'Отсутствует обязательное поле {error.args[0]}'},
+            status=status.HTTP_400_BAD_REQUEST,
+            )
+    except Exception as error:
+        return Response({'error': str(error)}, status=status.HTTP_400_BAD_REQUEST)
